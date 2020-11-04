b0eb79ad1bc1c56548ea58f20ba51536a32fddaa
Kiswari <kiswari10@gmail.com>
2020-11-04 15:20:57 -0500

    📦 add react-icons

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,4 +1,32 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+import { IconContext } from 'react-icons';
+import { AiOutlineSearch } from 'react-icons/ai';
+
+const SearchCont = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: 15px;
+  border-radius: 4px;
+  background: #ffffff;
+  border: 1px solid #11acf5;
+  .social_icon {
+    margin-left: 0.4rem;
+    margin-top: 0.2rem;
+    margin-right: 4%;
+  }
+`;
+const InputSearch = styled.input`
+  width: 200px;
+  border: none;
+  padding: 3px;
+  background: #ffffff;
+  :hover,
+  :focus,
+  :active {
+    outline: none;
+  }
+`;
 
 export const AddCategory = () => {
   const [inputValue, setInputValue] = useState("Hola mundo");
@@ -12,6 +40,18 @@ export const AddCategory = () => {
   return (
     <>
       <form onSubmit={handleSubmit}>
+        <SearchCont>
+          <IconContext.Provider
+            value={{
+              size: "1.6rem",
+              position: "absolute",
+              className: "social_icon",
+            }}
+          >
+            <AiOutlineSearch></AiOutlineSearch>
+          </IconContext.Provider>
+          <InputSearch type="text"/>
+        </SearchCont>
         <input value={inputValue} onChange={handleInputValue} />
       </form>
     </>
