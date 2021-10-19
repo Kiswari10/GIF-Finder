375ab42da04ad99e02bbd9010d15c4582540356b
kiswari10 <kiswari10@gmail.com>
2021-10-19 15:52:20 -0500

    ✅ add test in AddCategory: change value in input

--- src/tests/AddCategory.test.js
+++ src/tests/AddCategory.test.js
@@ -4,9 +4,19 @@ import { shallow } from "enzyme";
 import { AddCategory } from "../components/AddCategory";
 
 describe("Pruebas en <AddCategory />", () => {
+  const setCategories = () => {};
+  const wraper = shallow(<AddCategory setCategories={setCategories} />);
+
   test("debe de retornar el componente correctamente", () => {
-    const setCategories = () => {};
-    const wraper = shallow(<AddCategory setCategories={setCategories} />);
     expect(wraper).toMatchSnapshot();
+  });
+
+  test("debe cambiar el valor del input", () => {
+    const input = wraper.find("input");
+    const value = "Hello World";
+    input.simulate("change", { target: { value } });
+    const inputAfter = wraper.find("input");
+
+    expect(inputAfter.props().value).toBe(value);
   });
 });
