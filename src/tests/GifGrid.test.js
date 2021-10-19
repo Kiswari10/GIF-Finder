d81041c1e2defa42c77981204d9c5c1289f43729
kiswari10 <kiswari10@gmail.com>
2021-10-19 17:26:07 -0500

    🤡 ✅ add useFetchGifs mock data and test if component exist

--- src/tests/GifGrid.test.js
+++ src/tests/GifGrid.test.js
@@ -3,12 +3,34 @@ import "@testing-library/jest-dom";
 import { shallow } from "enzyme";
 
 import { GifGrid } from "../components/GifGrid";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+jest.mock("../hooks/useFetchGifs");
 
 describe("Pruebas en <GifGrid />", () => {
+  const category = "Goku";
+
   test("debe retornar el componente correctamente", () => {
-    const category = "Goku";
+    useFetchGifs.mockReturnValue({
+      gifs: [],
+      loading: true,
+    });
     const wraper = shallow(<GifGrid category={category} />);
 
     expect(wraper).toMatchSnapshot();
+  });
+
+  test("debe mostrar imagenes cuando se carga data con useFetchGifs", () => {
+    const gifs = [
+      { id: "123", url: "http://localhost/image.jpg", title: "Una imagen" },
+    ];
+    useFetchGifs.mockReturnValue({
+      gifs,
+      loading: false,
+    });
+    const wraper = shallow(<GifGrid category={category} />);
+
+    expect(wraper).toMatchSnapshot();
+    expect(wraper.find("Spinner").exists()).toBe(false);
+    expect(wraper.find("GifCard").length).toBe(gifs.length);
   });
 });
